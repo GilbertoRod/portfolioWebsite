aa468954e068e5ef8da8902362780013af64e45e
GilbertoRod <gilrod1017@gmail.com>
2024-05-09 13:28:06 -0500

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import './styles/Hero.css'
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className='intro'>
+                <div className='greeting'>
+                    Hello There! I'm Gilberto
+                </div>
+                <div className='hero-title'>
+                    <p>A <span>Software Engineer</span> In The Dallas-Fort Worth Area. Helping Companies And Individuals Launch Their Websites</p>
+                </div>
+                <div className='hero-about'>
+                    <p>I'm a 23 year old Software Engineer with a Bachelor's Degree in Computer Science. I'm experienced in working with individuals and businesses to help create their product websites. I also have experience in leading and collaborating with other software engineers to complete projects within given deadlines.</p>
+
+                </div>
+
+            </div>
+            <img src={require('../assets/Avatar.png')} alt='Avatar Pic' className='hero-img'/>
+
+        </div>
+    )
+}
+
+export default Hero

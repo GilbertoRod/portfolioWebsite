bd7b7e45006041939aa0b8a5956bac1d32c8f526
Gilberto Rodriguez <grodriguez17@leomail.tamuc.edu>
2024-05-11 02:06:34 -0500

    responsive nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import'./styles/Navbar.css'
+
 function Navbar() {
     return (
       <div className='nav-container'>
         <div className='navbar'>
         <p>GR</p>
         <ul>
-          <li>
-            Resume
-          </li>
-          <li>
-            Contact
-          </li>
+          <a href='https://drive.google.com/file/d/1nahaf3Nlh-BVS0Wz7I9zVJ5NOMHHKWso/view?usp=sharing' target='_blank'>
+            <li>
+              Resume
+            </li>
+          </a>
+          <a href="mailto:betovenr17@gmail.com" target="_blank">
+            <li>
+              Contact
+            </li>
+          </a>
         </ul>
         </div>
       </div>
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './styles/Technologies.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faCss3, faCss3Alt, faGitSquare, faHtml5, faJava, faJs, faMdb, faNodeJs, faPhp, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faCss3Alt, faGitAlt, faHtml5, faJs, faNodeJs, faReact, faPhp } from '@fortawesome/free-brands-svg-icons';
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 import { MongoIcon } from '../assets/MongoDbIcon';
+import { python } from '../assets/pythonIcon';
+import { java } from '../assets/javaIcon';
+import { flask } from '../assets/flaskIcon';
 
 
-function NameAndLogo({name,icon}){
+function NameAndLogo({name,icon,color}){
   if (name==="MongoDB"){
     return(
     <div className='technology'>
@@ -14,9 +17,30 @@ function NameAndLogo({name,icon}){
       <p>{name}</p>
     </div>)
   }
+  if (name==="Python"){
+    return(
+    <div className='technology'>
+      <div>{python}</div>
+      <p>{name}</p>
+    </div>)
+  }
+  if (name==="Java"){
+    return(
+    <div className='technology'>
+      <div>{java}</div>
+      <p>{name}</p>
+    </div>)
+  }
+  if (name==="Flask"){
+    return(
+    <div className='technology'>
+      <div>{flask}</div>
+      <p>{name}</p>
+    </div>)
+  }
   return(
     <div className='technology'>
-      <FontAwesomeIcon className='tech-icon' icon={icon}/>
+      <FontAwesomeIcon color={color} className='tech-icon' icon={icon}/>
       <p>{name}</p>
     </div>
   )
@@ -34,16 +58,19 @@ function Technologies() {
         </div>
 
         <div className='tech-logos'>
-          <NameAndLogo icon={faHtml5} name="HTML"/>
-          <NameAndLogo icon={faCss3Alt} name="CSS"/>
-          <NameAndLogo icon={faJs} name="JavaScript"/>
-          <NameAndLogo icon={faReact} name="React"/>
-          <NameAndLogo icon={faNodeJs} name="Node"/>
-          <NameAndLogo icon={faGitSquare} name="git"/>
-          <NameAndLogo icon={faDatabase} name="SQL"/>
+          <NameAndLogo icon={faHtml5} name="HTML" color='#dd4b25'/>
+          <NameAndLogo icon={faCss3Alt} name="CSS" color='#254bdd'/>
+          <NameAndLogo icon={faJs} name="JavaScript" color='#f0dc4e'/>
+          <NameAndLogo icon={faReact} name="React" color='#00d8ff'/>
+          <NameAndLogo icon={faNodeJs} name="Node" color='#529f41'/>
+          <NameAndLogo icon={faGitAlt} name="git" color='#f05030'/>
+          <NameAndLogo icon={faDatabase} name="SQL" color='#e06c1a'/>
           <NameAndLogo name="MongoDB"/>
-          <NameAndLogo icon={faPython} name="Python"/>
-          <NameAndLogo icon={faJava} name="Java"/>
+          <NameAndLogo name="Python"/>
+          <NameAndLogo name="Flask"/>
+          <NameAndLogo name="Java"/>
+          <NameAndLogo icon={faPhp} name="php" color='#7377ad'/>
+
           
         </div>
       </div>

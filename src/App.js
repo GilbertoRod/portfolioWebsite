0df3ada529c8f826dffe7c5119ae236068f2e295
Gilberto Rodriguez <grodriguez17@leomail.tamuc.edu>
2024-05-11 01:02:15 -0500

    responsive UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
@@ -6,7 +7,7 @@ import Technologies from "./components/Technologies";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <Hero/>
       <Technologies/>
       <Projects/>
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import'./styles/Navbar.css'
+function Navbar() {
+    return (
+      <div className='nav-container'>
+        <div className='navbar'>
+        <p>GR</p>
+        <ul>
+          <li>
+            Resume
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+        </div>
+      </div>
+    )
+}
+
+export default Navbar

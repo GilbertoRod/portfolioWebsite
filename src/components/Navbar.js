564e445e874a962282b2bb2e59547d2a7fa39700
GilbertoRod <gilrod1017@gmail.com>
2024-05-12 15:49:30 -0500

    hamburger animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import'./styles/Navbar.css'
+import React, { useState } from 'react';
+import './styles/Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered, faTimes } from '@fortawesome/free-solid-svg-icons'; // Changed faX to faTimes
 
 function Navbar() {
-    return (
+  const [sidebar, setSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+      setSidebar(!sidebar);
+  };
+
+  return (
       <div className='nav-container'>
-        <div className='navbar' data-aos='zoom-in-right' data-aos-duration='800' >
-        <p>GR</p>
-        <ul>
-          <a href='https://drive.google.com/file/d/1nahaf3Nlh-BVS0Wz7I9zVJ5NOMHHKWso/view?usp=sharing' target='_blank'  rel='noreferrer'>
-            <li>
-              Resume
-            </li>
-          </a>
-          <a href="mailto:betovenr17@gmail.com" target="_blank"  rel='noreferrer'>
-            <li>
-              Contact
-            </li>
-          </a>
-        </ul>
+          <div className='navbar' data-aos='zoom-in-right' data-aos-duration='800' >
+              {/* Your existing code */}
+              <div className='hamburger' onClick={toggleSidebar}>
+                  {!sidebar ? <FontAwesomeIcon icon={faBarsStaggered} /> :
+                      <div onClick={toggleSidebar}>
+                          <FontAwesomeIcon icon={faTimes} /> 
+                      </div>
+                  }
+              </div>
+              <p>GR</p>
+              <ul>
+                  <a href='https://drive.google.com/file/d/1nahaf3Nlh-BVS0Wz7I9zVJ5NOMHHKWso/view?usp=sharing' target='_blank' rel='noreferrer'>
+                      <li>
+                          Resume
+                      </li>
+                  </a>
+                  <a href="mailto:betovenr17@gmail.com" target="_blank" rel='noreferrer'>
+                      <li>
+                          Contact
+                      </li>
+                  </a>
+              </ul>
+
+              <div className={`sidebar ${sidebar ? 'active' : ''}`}>
+                      <a href='https://drive.google.com/file/d/1nahaf3Nlh-BVS0Wz7I9zVJ5NOMHHKWso/view?usp=sharing' target='_blank' rel='noreferrer'>
+                          <h1>Resume</h1>
+                        </a>
+
+                        <a href="mailto:betovenr17@gmail.com" target="_blank" rel='noreferrer'>
+                          <h1>Contact</h1>
+                        </a>
+                    </div>
+          
+            </div>
         </div>
-      </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;

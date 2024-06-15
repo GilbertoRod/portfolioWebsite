cb3889fece27fb4334d22927fe300f4d3fa43b2a
Gilberto Rodriguez <gilrod1017@gmail.com>
2024-06-15 00:09:49 -0500

    links updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,7 @@
 import './styles/Hero.css'
-
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons/faFile'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Hero() {
     return (
 
@@ -14,6 +16,8 @@ function Hero() {
                     </div>
                     <div className='hero-about'>
                         <p>I'm a 23 year old Software Engineer with a Bachelor's Degree in Computer Science. I'm experienced in working with individuals and businesses to help create their product websites. I also have experience in leading and collaborating with other software engineers to complete projects within given deadlines.</p>
+                        <a href='https://github.com/GilbertoRod'><button className='hero-github-btn'><FontAwesomeIcon className='svg-hero-icon' icon={faGithub}/> GitHub</button></a>
+                        <a href='https://drive.google.com/file/d/1MbkVH-ExzklaHaGDru2L3qjV-L_mJxDQ/view?usp=share_link'><button className='hero-resume-btn'><FontAwesomeIcon className='svg-hero-icon' icon={faFile}/> Resume</button></a>
 
                     </div>
 
